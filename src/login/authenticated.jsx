e20cc73331f40551f21cd604864396c31287464b
mistermyleswaite <mistermyleswaite@gmail.com>
2026-03-10 19:03:01 -0600

    fully implemented AuthState, first localStorage

---
+++ src/login/authenticated.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export function Authenticated(props) {
+    const navigate = useNavigate();
+
+    function logout() {
+        localStorage.removeItem('userName');
+        props.onLogout();
+    }
+
+    return (
+        <>
+        <p className='playerName'>Welcome, {props.userName}</p>
+        <div className='flex gap-20 m-4'>
+          <button onClick={() => navigate('/sort')()} className='btn-glass'>
+            Sort
+            </button>
+          <button onClick={() => logout()} className='btn-glass'>
+            Logout
+            </button>
+        </div>
+        </>
+    )
+}
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -10,6 +10,10 @@ function Login({ userName, authState, onAuthChange }) {
     <div className='homecontainer'>      
       <main className='content flex flex-wrap'>  
         <div>
+        {authState !== AuthState.Unknown}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}  
         {authState === AuthState.Unauthenticated && (
           <Unauthenticated
             userName={userName}
