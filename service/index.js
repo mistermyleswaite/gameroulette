0056a6c6e4a2c9d68d32e13b271b4033a07c12e2
mistermyleswaite <mistermyleswaite@gmail.com>
2026-03-13 19:18:18 -0600

    mocked up endpoints and updated userState func

--- service/index.js
+++ service/index.js
@@ -7,21 +7,129 @@ const app = express();
 
 const authCookieName = "token";
 
-app.use(cors()); 
+let users = [];
+let userStates = [];
+
 app.use(express.json());
 app.use(cookieParser());
-
-let users = [];
-let scores = [];
+app.use(express.static('public'));
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 let apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-app.get('/api/status', (_req, res) => {
-  res.send({ msg: 'Startup service is active' });
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
 });
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+// GetsortedGames
+apiRouter.get('/lists/get', verifyAuth, (_req, res) => {
+  res.send(lists);
+});
+
+// SubmitScore
+apiRouter.post('/lists', verifyAuth, (req, res) => {
+  lists = updateLists(req.body);
+  res.send(lists);
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+function updateUserData(email, unsorted, sorted) {
+  const index = userStates.findIndex((u) => u.email === email);
+  
+  const newState = {
+    email: email,
+    unsorted: unsorted || [],
+    sorted: sorted || { UPNX: [], ALPD: [], BKLG: [], PTOD: [] },
+  };
+
+  if (index !== -1) {
+    userStates[index] = newState;
+  } else {
+    userStates.push(newState);
+  }
+  return newState;
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is holding the line on port ${port}`);
