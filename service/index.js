7c9f51f238fc6500fd7f092618f28d95409c736a
mistermyleswaite <mistermyleswaite@gmail.com>
2026-03-26 21:59:47 -0600

    commented out unused functions, updated db paths

--- service/index.js
+++ service/index.js
@@ -17,11 +17,10 @@ let db;
 
 
 const app = express();
-
 const authCookieName = "token";
 
-let users = [];
-let userStates = [];
+// let users = [];
+// let userStates = [];
 
 app.use(express.json());
 app.use(cookieParser());
@@ -34,10 +33,15 @@ app.use(`/api`, apiRouter);
 
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  if (await findUser('email', req.body.email)) {
+  if (await db.collection('users').findOne({ email: req.body.email })) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = await createUser(req.body.email, req.body.password);
+    const user = {
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10),
+      token: uuid.v4()
+    };
+    await db.collection('users').insertOne(user);
 
     setAuthCookie(res, user.token);
     res.send({ email: user.email });
@@ -46,11 +50,12 @@ apiRouter.post('/auth/create', async (req, res) => {
 
 // GetAuth login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
-  const user = await findUser('email', req.body.email);
-  if (user) {
+  const user = await db.collection('users').findOne({ email: req.body.email });
+  if (user && await bcrypt.compare(req.body.password, user.password)) {
     if (await bcrypt.compare(req.body.password, user.password)) {
-      user.token = uuid.v4();
-      setAuthCookie(res, user.token);
+      const newToken = uuid.v4();
+
+      setAuthCookie(res, newToken);
       res.send({ email: user.email });
       return;
     }
@@ -60,9 +65,12 @@ apiRouter.post('/auth/login', async (req, res) => {
 
 // DeleteAuth logout a user
 apiRouter.delete('/auth/logout', async (req, res) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
+  const user = await db.collection('users').findOne({ token: req.cookies[authCookieName] });
   if (user) {
-    delete user.token;
+    await db.collection('users').updateOne(
+      { _id: user._id },
+      { $unset: { token: "" } }
+    );
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
@@ -70,8 +78,9 @@ apiRouter.delete('/auth/logout', async (req, res) => {
 
 // Middleware to verify that the user is authorized to call an endpoint
 const verifyAuth = async (req, res, next) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
+  const user = await db.collection('users').findOne({ token: req.cookies[authCookieName] });
   if (user) {
+    req.user = user;
     next();
   } else {
     res.status(401).send({ msg: 'Unauthorized' });
@@ -81,13 +90,13 @@ const verifyAuth = async (req, res, next) => {
 // get userState - only pulls the isolated user's state
 apiRouter.get('/lists/get', verifyAuth, async (req, res) => {
   try {
-    const user = await findUser('token', req.cookies[authCookieName]);
+    const user = await db.collection('users').findOne({ token: req.cookies[authCookieName] });
 
     if (!user) {
       return res.status(401).send( {msg: 'Unauthorized!' });
     }
     
-    const state = userStates.find((u) => u.email === user.email);
+    const state = await db.collection('lists').findOne({ email: req.user.email });
 
     if (state) {
         res.send(state);
@@ -106,10 +115,14 @@ apiRouter.get('/lists/get', verifyAuth, async (req, res) => {
 
 // post userState - isolates only the current user to update their state
 apiRouter.post('/lists', verifyAuth, async (req, res) => {
-  const user = await findUser('token', req.cookies[authCookieName]);
   const { unsorted, sorted } = req.body;
-  const newState = updateUserStates(user.email, unsorted, sorted);
-  res.send(newState);
+  await db.collection('lists').updateOne(
+    { email: req.user.email },
+    { $set: {email: req.user.email, unsorted, sorted } },
+    { upsert: true }  
+  );
+
+  res.send({unsorted, sorted });
 });
 
 // Default error handler
@@ -122,41 +135,41 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-function updateUserStates(email, unsorted, sorted) {
-  const index = userStates.findIndex((u) => u.email === email);
+// function updateUserStates(email, unsorted, sorted) {
+//   const index = userStates.findIndex((u) => u.email === email);
   
-  const newState = {
-    email: email,
-    unsorted: unsorted || [],
-    sorted: sorted || { UPNX: [], ALPD: [], BKLG: [], PTOD: [] },
-  };
+//   const newState = {
+//     email: email,
+//     unsorted: unsorted || [],
+//     sorted: sorted || { UPNX: [], ALPD: [], BKLG: [], PTOD: [] },
+//   };
 
-  if (index !== -1) {
-    userStates[index] = newState;
-  } else {
-    userStates.push(newState);
-  }
-  return newState;
-}
+//   if (index !== -1) {
+//     userStates[index] = newState;
+//   } else {
+//     userStates.push(newState);
+//   }
+//   return newState;
+// }
 
-async function createUser(email, password) {
-  const passwordHash = await bcrypt.hash(password, 10);
+// async function createUser(email, password) {
+//   const passwordHash = await bcrypt.hash(password, 10);
 
-  const user = {
-    email: email,
-    password: passwordHash,
-    token: uuid.v4(),
-  };
-  users.push(user);
+//   const user = {
+//     email: email,
+//     password: passwordHash,
+//     token: uuid.v4(),
+//   };
+//   users.push(user);
 
-  return user;
-}
+//   return user;
+// }
 
-async function findUser(field, value) {
-  if (!value) return null;
+// async function findUser(field, value) {
+//   if (!value) return null;
 
-  return users.find((u) => u[field] === value);
-}
+//   return users.find((u) => u[field] === value);
+// }
 
 // setAuthCookie in the HTTP response
 function setAuthCookie(res, authToken) {
