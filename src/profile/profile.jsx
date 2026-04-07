7d0d66860322edb47f113e4a4bc148e2b0834c20
mistermyleswaite <mistermyleswaite@gmail.com>
2026-04-07 17:04:41 -0600

    can now import and save loaded games!!!

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,32 +1,84 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useNavigate } from 'react-router';
 import { AuthState } from '../login/authState';
 
 function Profile({ authState }) {
-  const navigate = useNavigate();
-  const [steamId, setSteamId] = useState('');
+    const navigate = useNavigate();
 
-  async function importSteamGames() {
-    if (!steamId) return alert("please enter a SteamID64");
+    const isLoaded = useRef(false);
 
-    try {
-        const response = await fetch(`/api/stea/test/${steamId}`);
-        const data = await response.json();
+    const [steamId, setSteamId] = useState('');
+    const [unsortedGames, setUnsortedGames] = useState([]);
+    const [sortedGames, setSortedGames] = useState({
+          UPNX: [],
+          ALPD: [],
+          BKLG: [],
+          PTOD: []
+    });
 
-        if (data.success) {
-            const importedGames = data.games.map(g => ({
-                id: g.appid,
-                name: g.name,
-                img: g.img_icon_url
-            }));
+    async function loadInitialData() {
+            if (isLoaded.current) return;
 
-            setUnsortedGames(prev => {
-                const existingIds = new Set(prev.map(game => game.id));
+            try {
+                const response = await fetch('/api/lists/get', {
+                    method: 'GET',
+                    credentials: 'include',
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setUnsortedGames(data.unsorted || []);
+                    setSortedGames(data.sorted || { UPNX: [], ALPD: [], BKLG: [], PTOD: [] });
+                    isLoaded.current = true;
+                }
+
+            } catch (error) {
+                console.error("Error loading games:", error);
+            }
+    }
+
+    useEffect(() => {
+        if (authState === AuthState.Unauthenticated || authState === AuthState.Unknown) {
+            navigate('/');
+        }
+        loadInitialData();
+    }, [authState, navigate])
+
+    async function importSteamGames() {
+        if (!steamId) return alert("please enter a SteamID64");
+
+        try {
+            const response = await fetch(`/api/steam/test/${steamId}`);
+            const data = await response.json();
+
+            if (data.success) {
+                const importedGames = data.games.map(g => ({
+                    id: g.appid,
+                    name: g.name,
+                    img: g.img_icon_url
+                }));
+
+                const existingIds = new Set(unsortedGames.map(game => game.id));
                 const newGames = importedGames.filter(g => !existingIds.has(g.id));
-                return [...prev, ...newGames];
-            });
 
-            alert(`Successfully imported ${data.count} games!`)
+                const updatedUnsortedList = [...unsortedGames, ...newGames];
+
+                setUnsortedGames(updatedUnsortedList);
+
+                try {
+                    await fetch('/api/lists', {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            unsorted: updatedUnsortedList,
+                            sorted: sortedGames
+                        }),
+                        credentials: 'include',
+                    });
+                } catch (error) {
+                    console.error("Save Error:", error);
+                }
+                alert(`Successfully imported ${data.count} games!`)
         } else {
             alert(data.msg);
         }
@@ -35,20 +87,14 @@ function Profile({ authState }) {
     }
   }
 
-  useEffect(() => {
-        if (authState === AuthState.Unauthenticated || authState === AuthState.Unknown) {
-            navigate('/');
-        }
-    }, [authState, navigate])
-
   return (
     <main className='container'>
         <div className='profile-content'>
             <h3>Account Information</h3>
             <ul>
-                <li><p>Username: "[user]" </p></li>
-                <li><p>SteamID: "[SteamID]" </p></li>
-                <li><p>Lists: "<a href="lists.html">[Lists]</a>"</p></li>
+                <li><p>Username: {localStorage.user} </p></li>
+                <li><p>SteamID64: {steamId} </p></li>
+                <li><p>Lists: <a href="lists.html">[Lists]</a>"</p></li>
             </ul>
         </div>
         <div className="steam-import-section card p-3 mt-4">
@@ -57,7 +103,7 @@ function Profile({ authState }) {
                 <label htmlFor="steamID">SteamID64</label>
                 <input type="text" className='input-glass' placeholder="e.g. 7485994..." value={steamId} onChange={(e) => setSteamId(e.target.value)} />
                 <small className="text-muted">
-                    Find your ID at <a href="https;//steamid.io" target="_blank" rel="noreferrer">steamid.io</a>.
+                    Find your ID at <a href="https://steamid.io" target="_blank" rel="noreferrer">steamid.io</a>.
                     Make sure your <b>Game Details</b> are set to <b>Public</b> in Steam privacy settings.
                 </small>
             </div>
